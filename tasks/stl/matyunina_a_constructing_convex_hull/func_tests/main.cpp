22987e0fbb435bd66bcca932fa29c98b8d0a054c
erty1909 <ERTY-00@yandex.ru>
2025-09-11 15:05:36 +0300

    fix bag

--- tasks/stl/matyunina_a_constructing_convex_hull/func_tests/main.cpp
+++ tasks/stl/matyunina_a_constructing_convex_hull/func_tests/main.cpp
@@ -6,9 +6,9 @@
 #include <memory>
 #include <string>
 #include <vector>
-// #ifndef _WIN32
-// #include <opencv2/opencv.hpp>
-// #endif
+#ifndef _WIN32
+#include <opencv2/opencv.hpp>
+#endif
 
 #include "core/task/include/task.hpp"
 #include "core/util/include/util.hpp"
@@ -42,106 +42,106 @@ void RunTest(int height, int width, std::vector<int> image,
   EXPECT_EQ(points, ans);
 }
 
-// #ifndef _WIN32
-// bool RunImageTest(std::string image_path, std::string ans_path) {
-//   cv::Mat image = cv::imread(image_path);
-//   if (image.empty()) {
-//     return false;
-//   }
-//   cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
-//   cv::threshold(image, image, 128, 1, cv::THRESH_BINARY_INV);
+#ifndef _WIN32
+bool RunImageTest(std::string image_path, std::string ans_path) {
+  cv::Mat image = cv::imread(image_path);
+  if (image.empty()) {
+    return false;
+  }
+  cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+  cv::threshold(image, image, 128, 1, cv::THRESH_BINARY_INV);
 
-//   std::vector<int> pixels(image.rows * image.cols);
-//   for (int i = 0; i < image.rows; i++) {
-//     for (int j = 0; j < image.cols; j++) {
-//       pixels[(i * image.cols + j)] = image.at<uchar>(i, j);
-//     }
-//   }
+  std::vector<int> pixels(image.rows * image.cols);
+  for (int i = 0; i < image.rows; i++) {
+    for (int j = 0; j < image.cols; j++) {
+      pixels[(i * image.cols + j)] = image.at<uchar>(i, j);
+    }
+  }
 
-//   int width = image.cols;
-//   int height = image.rows;
+  int width = image.cols;
+  int height = image.rows;
 
-//   auto task_data = std::make_shared<ppc::core::TaskData>();
-//   task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(pixels.data()));
-//   task_data->inputs_count.emplace_back(width);
-//   task_data->inputs_count.emplace_back(height);
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(pixels.data()));
+  task_data->inputs_count.emplace_back(width);
+  task_data->inputs_count.emplace_back(height);
 
-//   matyunina_a_constructing_convex_hull_stl::ConstructingConvexHull constructingConvexHull(task_data);
-//   constructingConvexHull.Validation();
-//   constructingConvexHull.PreProcessing();
-//   constructingConvexHull.Run();
-//   constructingConvexHull.PostProcessing();
+  matyunina_a_constructing_convex_hull_stl::ConstructingConvexHull constructingConvexHull(task_data);
+  constructingConvexHull.Validation();
+  constructingConvexHull.PreProcessing();
+  constructingConvexHull.Run();
+  constructingConvexHull.PostProcessing();
 
-//   matyunina_a_constructing_convex_hull_stl::Point* pointArray =
-//       reinterpret_cast<matyunina_a_constructing_convex_hull_stl::Point*>(task_data->outputs[0]);
-//   std::vector<matyunina_a_constructing_convex_hull_stl::Point> points(pointArray,
-//                                                                       pointArray + task_data->outputs_count[0]);
+  matyunina_a_constructing_convex_hull_stl::Point* pointArray =
+      reinterpret_cast<matyunina_a_constructing_convex_hull_stl::Point*>(task_data->outputs[0]);
+  std::vector<matyunina_a_constructing_convex_hull_stl::Point> points(pointArray,
+                                                                      pointArray + task_data->outputs_count[0]);
 
-//   // std::cout << "\n#######\n";
-//   // for (matyunina_a_constructing_convex_hull_stl::Point& point : points) {
-//   //   std::cout << "x: " << point.x << " y: " << point.y << "\n";
-//   // }
-//   // std::cout << "\n#######\n";
+  // std::cout << "\n#######\n";
+  // for (matyunina_a_constructing_convex_hull_stl::Point& point : points) {
+  //   std::cout << "x: " << point.x << " y: " << point.y << "\n";
+  // }
+  // std::cout << "\n#######\n";
 
-//   cv::Mat ans = cv::imread(ans_path);
-//   if (ans.empty()) {
-//     return false;
-//   }
-//   std::vector<matyunina_a_constructing_convex_hull_stl::Point> ansPoints;
+  cv::Mat ans = cv::imread(ans_path);
+  if (ans.empty()) {
+    return false;
+  }
+  std::vector<matyunina_a_constructing_convex_hull_stl::Point> ansPoints;
 
-//   for (int i = 0; i < ans.cols; i++) {
-//     for (int j = 0; j < ans.rows; j++) {
-//       cv::Vec3b pixel = ans.at<cv::Vec3b>(j, i);
-//       uchar blue = pixel[0];
-//       uchar green = pixel[1];
-//       uchar red = pixel[2];
+  for (int i = 0; i < ans.cols; i++) {
+    for (int j = 0; j < ans.rows; j++) {
+      cv::Vec3b pixel = ans.at<cv::Vec3b>(j, i);
+      uchar blue = pixel[0];
+      uchar green = pixel[1];
+      uchar red = pixel[2];
 
-//       if (red > 200 && green < 50 && blue < 50) {
-//         ansPoints.push_back({i, j});
-//       }
-//     }
-//   }
+      if (red > 200 && green < 50 && blue < 50) {
+        ansPoints.push_back({i, j});
+      }
+    }
+  }
 
-//   image.release();
-//   ans.release();
+  image.release();
+  ans.release();
 
-//   return ansPoints == points;
-// }
-// #endif
+  return ansPoints == points;
+}
+#endif
 }  // namespace
 
-// #ifndef _WIN32
-// TEST(matyunina_a_constructing_convex_hull_stl, image1) {
-//   std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image1.png");
-//   std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans1.png");
+#ifndef _WIN32
+TEST(matyunina_a_constructing_convex_hull_stl, image1) {
+  std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image1.png");
+  std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans1.png");
 
-//   ASSERT_TRUE(RunImageTest(src_path, exp_path));
-// }
-// TEST(matyunina_a_constructing_convex_hull_stl, image2) {
-//   std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image2.png");
-//   std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans2.png");
+  ASSERT_TRUE(RunImageTest(src_path, exp_path));
+}
+TEST(matyunina_a_constructing_convex_hull_stl, image2) {
+  std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image2.png");
+  std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans2.png");
 
-//   ASSERT_TRUE(RunImageTest(src_path, exp_path));
-// }
-// TEST(matyunina_a_constructing_convex_hull_stl, image3) {
-//   std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image3.png");
-//   std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans3.png");
+  ASSERT_TRUE(RunImageTest(src_path, exp_path));
+}
+TEST(matyunina_a_constructing_convex_hull_stl, image3) {
+  std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image3.png");
+  std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans3.png");
 
-//   ASSERT_TRUE(RunImageTest(src_path, exp_path));
-// }
-// TEST(matyunina_a_constructing_convex_hull_stl, image4) {
-//   std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image4.png");
-//   std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans4.png");
+  ASSERT_TRUE(RunImageTest(src_path, exp_path));
+}
+TEST(matyunina_a_constructing_convex_hull_stl, image4) {
+  std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image4.png");
+  std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans4.png");
 
-//   ASSERT_TRUE(RunImageTest(src_path, exp_path));
-// }
-// TEST(matyunina_a_constructing_convex_hull_stl, image5) {
-//   std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image5.png");
-//   std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans5.png");
+  ASSERT_TRUE(RunImageTest(src_path, exp_path));
+}
+TEST(matyunina_a_constructing_convex_hull_stl, image5) {
+  std::string src_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/image5.png");
+  std::string exp_path = ppc::util::GetAbsolutePath("stl/matyunina_a_constructing_convex_hull/data/ans5.png");
 
-//   ASSERT_TRUE(RunImageTest(src_path, exp_path));
-// }
-// #endif
+  ASSERT_TRUE(RunImageTest(src_path, exp_path));
+}
+#endif
 
 TEST(matyunina_a_constructing_convex_hull_stl, test_9_9) {
   int height = 9;
